c7587ce409f38092761580fedc0d88d3e9b48eb7
Josh Wilsdon <jwilsdon@joyent.com>
2010-11-24 15:19:12 -0800

    Make it possible to get more debug messages out of mount_usb

--- overlay/lib/svc/share/joyent_include.sh
+++ overlay/lib/svc/share/joyent_include.sh
@@ -6,8 +6,14 @@ MOUNTPOINT=/mnt
 
 mount_usb() {
 
+    mount_usb_msg=""
+
+    if [ -z "${DEBUG}" ] || [ "${DEBUG}" != "true" ]; then
+        DEBUG="false"
+    fi
+
     if [ -f "/mnt/.joyliveusb" ]; then
-        # Already mounted
+        mount_usb_msg="already mounted"
         return 0;
     fi
 
@@ -18,11 +24,21 @@ mount_usb() {
             /usr/sbin/mount -F pcfs /dev/dsk/${key}p0:1 /mnt;
             if [[ $? == 0 ]]; then
                 if [[ ! -f /mnt/.joyliveusb ]]; then
+                    if [ "${DEBUG}" == "true" ]; then
+                        mount_usb_msg="${mount_usb_msg}\n[debug] didn't find /mnt/.joyliveusb on pcfs /dev/dsk/${key}p0:1"
+                    fi
                     /usr/sbin/umount /mnt;
                 else
+                    if [ "${DEBUG}" == "true" ]; then
+                        mount_usb_msg="${mount_usb_msg}\n[debug] found /mnt/.joyliveusb on pcfs /dev/dsk/${key}p0:1"
+                    fi
                     break;
                 fi
+            elif [ "${DEBUG}" == "true" ]; then
+                mount_usb_msg="${mount_usb_msg}\n[debug] mount failed for pcfs /dev/dsk/${key}p0:1"
             fi
+        elif [ "${DEBUG}" == "true" ]; then
+            mount_usb_msg="${mount_usb_msg}\n[debug] /dev/dsk/${key}p0:1 is not pcfs"
         fi
     done
 
@@ -33,19 +49,30 @@ mount_usb() {
                 /usr/sbin/mount -F pcfs /dev/dsk/${disk}p1 /mnt;
                 if [[ $? == 0 ]]; then
                     if [[ ! -f /mnt/.joyliveusb ]]; then
+                        if [ "${DEBUG}" == "true" ]; then
+                            mount_usb_msg="${mount_usb_msg}\n[debug] didn't find /mnt/.joyliveusb on pcfs /dev/dsk/${disk}p1"
+                        fi
                         /usr/sbin/umount /mnt;
                     else
+                        if [ "${DEBUG}" == "true" ]; then
+                            mount_usb_msg="${mount_usb_msg}\n[debug] found /mnt/.joyliveusb on pcfs /dev/dsk/${disk}p1"
+                        fi
                         break;
                     fi
+                elif [ "${DEBUG}" == "true" ]; then
+                    mount_usb_msg="${mount_usb_msg}\n[debug] unable to mount /dev/dsk/${disk}p1"
                 fi
+            elif [ "${DEBUG}" == "true" ]; then
+                mount_usb_msg="${mount_usb_msg}\n[debug] /dev/dsk/${disk}p1 is not pcfs"
             fi
         done
     fi
 
     if [ -f "/mnt/.joyliveusb" ]; then
+        mount_usb_msg="success"
         return 0;
     fi
 
-    echo "FATAL: Cannot find USB key" >/dev/console
+    mount_usb_msg="${mount_usb_msg}\n[FATAL] mount_usb(): could not find USB Key"
     return 1;
 }

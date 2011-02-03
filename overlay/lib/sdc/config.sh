66cff4ed27b1759e3d9c5a04e214ab3af74edd86
Josh Wilsdon <jwilsdon@joyent.com>
2011-02-03 14:36:43 -0800

    [HEAD-146 + HEAD-111] Add support for 'compute node' config + JSON output for config.sh

--- overlay/lib/sdc/config.sh
+++ overlay/lib/sdc/config.sh
@@ -3,21 +3,41 @@
 # functions for getting USB headnode config values (intended to be sourced
 # from other scripts)
 #
-# Copyright (c) 2010 Joyent Inc., All rights reserved.
+# It is also possible to use this to get a JSON hash of the config options using:
+#
+# bash config.sh -json
+#
+# Copyright (c) 2010,2011 Joyent Inc., All rights reserved.
 #
 
-
-# Loads sysinfo variables with SYSINFO_ prefix
+# Loads sysinfo variables with prefix (default: SYSINFO_)
 function load_sdc_sysinfo {
-    eval $(/usr/bin/sysinfo -p | sed -e "s/^/SYSINFO_/")
+
+    prefix=$1
+    [[ -z ${prefix} ]] && prefix="SYSINFO_"
+
+    eval $(/usr/bin/sysinfo -p | sed -e "s/^/${prefix}/")
 }
 
-# Sets SDC_CONFIG_FILENAME with the location of the USB config file
+# Sets SDC_CONFIG_FILENAME with the location of the USB config file, or /opt/smartdc/config
 function load_sdc_config_filename {
+
+    # the default
+    COMPUTE_NODE_CONFIG_FILENAME="/opt/smartdc/config/node.config"
+
     if [[ -z "${SDC_CONFIG_FILENAME}" ]]; then
         SDC_CONFIG_FILENAME="$(svcprop -p 'joyentfs/usb_copy_path' svc:/system/filesystem/smartdc:default)/config"
         if [[ ! -f ${SDC_CONFIG_FILENAME} ]]; then
             SDC_CONFIG_FILENAME="/mnt/$(svcprop -p 'joyentfs/usb_mountpoint' svc:/system/filesystem/smartdc:default)/config"
+        fi
+
+        if [[ -f ${COMPUTE_NODE_CONFIG_FILENAME} ]]; then
+            if [[ -f ${SDC_CONFIG_FILENAME} ]]; then
+                # We write to console to make it clear we don't like this at all.
+                echo "WARNING: ignoring config at ${COMPUTE_NODE_CONFIG_FILENAME} since we have ${SDC_CONFIG_FILENAME}" >> /dev/msglog
+            else
+                SDC_CONFIG_FILENAME=${COMPUTE_NODE_CONFIG_FILENAME}
+            fi
         fi
     fi
 }
@@ -40,8 +60,20 @@ function sdc_config_keys_contain {
     fi
 }
 
-# Loads config variables with CONFIG_ prefix
+function sdc_config_keys {
+    load_sdc_config_filename
+    if [[ -f ${SDC_CONFIG_FILENAME} ]]; then
+        keys=$(cat ${SDC_CONFIG_FILENAME} | sed -e "s/^ *//" | grep -v "^#" | grep "^[a-zA-Z]" | sed -e "s/=.*//")
+    fi
+    echo "${keys}"
+}
+
+# Loads config variables with prefix (default: CONFIG_)
 function load_sdc_config {
+
+    prefix=$1
+    [[ -z ${prefix} ]] && prefix="CONFIG_"
+
     if /bin/bootparams | grep "^headnode=true" >/dev/null; then
         headnode="true"
     else
@@ -52,10 +84,31 @@ function load_sdc_config {
         load_sdc_config_filename
         # Ignore comments,  spaces at the beginning of lines and lines that don't start with a letter.
         if [[ -f ${SDC_CONFIG_FILENAME} ]]; then
-            eval $(cat ${SDC_CONFIG_FILENAME} | sed -e "s/^ *//" | grep -v "^#" | grep "^[a-zA-Z]" | sed -e "s/^/CONFIG_/")
+            eval $(cat ${SDC_CONFIG_FILENAME} | sed -e "s/^ *//" | grep -v "^#" | grep "^[a-zA-Z]" | sed -e "s/^/${prefix}/")
         else
             echo "FATAL: Unable to load headnode config."
             exit 1
         fi
     fi
 }
+
+if [[ $1 == "-json" ]]; then
+    # If called to output config as JSON, we'll do that.
+    (
+        echo "{"
+        load_sdc_config
+        first_key=1
+        keys=$(sdc_config_keys)
+        for key in ${keys}; do
+            value=$(eval "echo \${CONFIG_${key}}")
+            # too bad we can't use extra commas
+            if [[ ${first_key} -eq 1 ]]; then
+                echo "    \"${key}\": \"${value}\""
+                first_key=0
+            else
+                echo "  , \"${key}\": \"${value}\""
+            fi
+        done
+        echo "}"
+    )
+fi

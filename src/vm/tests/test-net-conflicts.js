99a1e149d7da92ea6b816e72535d2593128af66f
Josh Wilsdon <jwilsdon@joyent.com>
2013-10-20 00:23:44 -0700

    OS-2541 ignore failed VMs that are also stopped when checking for duplicate IP/MAC/VRID

--- src/vm/tests/test-net-conflicts.js
+++ src/vm/tests/test-net-conflicts.js
@@ -4,6 +4,7 @@
 //
 
 process.env['TAP'] = 1;
+var execFile = require('child_process').execFile;
 var test = require('tap').test;
 var VM = require('/usr/vm/node_modules/VM');
 var vmtest = require('../common/vmtest.js');
@@ -66,6 +67,44 @@ should_fail_with_conflict('KVM with same IP and MAC',
         {'nic_tag': 'admin', 'mac': '0f:0e:0d:0c:0b:0a', 'ip': '172.17.2.173', 'model': 'virtio', 'netmask': '255.255.255.0'}]},
     {'brand': 'kvm', 'nics': [{'nic_tag': 'admin', 'mac': '01:02:03:04:05:06', 'ip': '172.17.2.172', 'model': 'virtio', 'netmask': '255.255.255.0'}]}
 );
+
+// test that we *don't* fail when conflict is with an already failed VM
+test('no conflict when reusing IP of failed VM', {'timeout': 240000}, function (t) {
+    var p;
+    var state;
+
+    var payloadA = {'brand': 'joyent-minimal', 'image_uuid': image_uuid, 'nics': [{'nic_tag': 'admin', 'mac': '01:02:03:04:05:06', 'ip': '172.17.2.172', 'netmask': '255.255.255.0'}]};
+    var payloadB = {'brand': 'joyent-minimal', 'image_uuid': image_uuid, 'nics': [{'nic_tag': 'admin', 'mac': '0f:0e:0d:0c:0b:0a', 'ip': '172.17.2.172', 'netmask': '255.255.255.0'}]};
+
+    for (p in payload) {
+        payloadA[p] = payload[p];
+        payloadB[p] = payload[p];
+    }
+
+    state = {'brand': payloadA.brand};
+    vmtest.on_new_vm(t, payloadA.image_uuid, payloadA, state, [
+        function (cb) {
+            execFile('/usr/sbin/zonecfg', ['-z', state.uuid, 'remove -F attr name=failed; add attr; set name=failed; set value="provisioning"; set type=string; end'],
+                function (error, stdout, stderr) {
+
+                if (error) {
+                    t.ok(false, 'failed to set failure flag on ' + state.uuid + ': ' + stderr);
+                    cb(error);
+                    return;
+                }
+                cb();
+            });
+        }, function (cb) {
+            vmtest.on_new_vm(t, payloadB.image_uuid, payloadB,
+                {'brand': payloadB.brand}, [],
+                function (err) {
+                    t.ok(!err, 'no error adding duplicate NIC with VM-A failed: ' + (err ? err.message : 'success'));
+                    cb();
+                }
+            );
+        }
+    ]);
+});
 
 // test that we can't have two nics with the same IP or MAC
 test('2 nics with same MAC', {'timeout': 240000}, function(t) {

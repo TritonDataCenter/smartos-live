b1bd30e00d106d855dcd094dd22e2bf8f00d08ba
Trent Mick <trentm@gmail.com>
2015-03-05 12:25:05 -0800

    OS-3998: update bunyan to latest (1.3.4)

--- src/node-bunyan/examples/handle-fs-error.js
+++ src/node-bunyan/examples/handle-fs-error.js
@@ -3,7 +3,7 @@
 
 var fs = require('fs');
 var path = require('path');
-var Logger = require('../lib/bunyan');
+var bunyan = require('../lib/bunyan');
 
 var FILENAME = 'handle-fs-error.log';
 var S_IWUSR = 00200; // mask for owner write permission in stat mode
@@ -18,19 +18,24 @@ if (fs.statSync(FILENAME).mode & S_IWUSR) {
         fs.chmodSync(FILENAME, 0444);
 }
 
-console.warn('- Create logger.')
-var log = new Logger({name: 'handle-fs-error', streams: [{path: FILENAME}]});
+console.warn('- Create logger.');
+var log = bunyan.createLogger({
+        name: 'handle-fs-error',
+        streams: [
+                  {path: FILENAME}
+        ]
+});
 
 log.on('error', function (err) {
         console.warn('- The logger emitted an error:', err);
 });
 
-console.warn('- Call log.info(...).')
+console.warn('- Call log.info(...).');
 log.info('info log message');
-console.warn('- Called log.info(...).')
+console.warn('- Called log.info(...).');
 
 setTimeout(function () {
-        console.warn('- Call log.warn(...).')
+        console.warn('- Call log.warn(...).');
         log.warn('warn log message');
-        console.warn('- Called log.warn(...).')
+        console.warn('- Called log.warn(...).');
 }, 1000);

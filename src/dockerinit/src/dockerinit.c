5803aa8f3f3f96aa117602e8e0a02eafb7326e24
Josh Wilsdon <jwilsdon@joyent.com>
2016-03-18 05:56:04 +0000

    initial prototype of nfs volumes mounting

--- src/dockerinit/src/dockerinit.c
+++ src/dockerinit/src/dockerinit.c
@@ -77,6 +77,7 @@
 #define DOCKER_LOGGER "/lib/sdc/docker/logger"
 #define IPMGMTD "/lib/inet/ipmgmtd"
 #define IPMGMTD_DOOR "/etc/svc/volatile/ipadm/ipmgmt_door"
+#define NFS_MOUNT "/usr/lib/fs/nfs/mount"
 
 #define LOGFILE "/var/log/sdc-dockerinit.log"
 #define RTMBUFSZ sizeof (struct rt_msghdr) + (3 * sizeof (struct sockaddr_in))
@@ -97,6 +98,10 @@ void runIpmgmtd(void);
 void setupHostname();
 void setupInterface(nvlist_t *data);
 void setupInterfaces();
+void workaround_OS_5221();
+void doNfsMount(const char *nfsvolume, const char *mountpoint);
+void mountNfsVolume(nvlist_t *data);
+void mountNfsVolumes();
 static void makeMux(int stdid, int logid, boolean_t use_flowcon);
 static void setupTerminal(boolean_t ctty);
 static void setupLogging(boolean_t ctty);
@@ -1224,6 +1229,166 @@ setupNetworking()
     closeIpadmHandle();
 }
 
+// HACK to workaround OS-5221, create /etc/nfssec.conf
+void
+workaround_OS_5221()
+{
+    const char *data = "sys\t1\t-\t-\t-\n";
+    int fd;
+    int ret;
+
+    fd = open("/etc/nfssec.conf", O_WRONLY | O_CREAT | O_EXCL,
+        S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+
+    if (fd == -1) {
+        if (errno == EEXIST) {
+            dlog("DEBUG nfssec.conf already exists\n");
+            return;
+        }
+        fatal(ERR_OPEN, "failed to open nfssec.conf: (%d) %s\n",
+            errno, strerror(errno));
+    }
+
+    ret = write(fd, data, strlen(data) * sizeof(char));
+    if (ret == -1) {
+        // XXX not an ERR_OPEN really, but this is just a temp hack.
+        fatal(ERR_OPEN, "failed to write nfssec.conf: (%d) %s\n",
+            errno, strerror(errno));
+    }
+
+    close(fd);
+}
+
+/*
+ * TODO: Eventually this impl should be replaced by calls to mount(2) and
+ * friends instead, but this currently requires additional work for things like
+ * parsing the address. For this prototype we'll just execv() the mount.
+ */
+void
+doNfsMount(const char *nfsvolume, const char *mountpoint)
+{
+    pid_t pid;
+    int status;
+    int ret;
+    int tmplfd;
+
+    dlog("INFO mounting %s on %s\n", nfsvolume, mountpoint);
+
+    /* ensure the directory exists */
+    ret = mkdir(mountpoint, 0755);
+    if (ret == -1 && errno != EEXIST) {
+        fatal(ERR_MKDIR, "failed to mkdir(%s): (%d) %s\n", mountpoint,
+            errno, strerror(errno));
+    }
+
+    /* do the mount */
+
+    tmplfd = init_template(0);
+
+    if ((pid = fork()) == -1) {
+        fatal(ERR_FORK_FAILED, "fork() failed: %s\n", strerror(errno));
+    }
+
+    if (pid == 0) {
+        /* child */
+        char cmd[MAXPATHLEN];
+        char *const argv[] = {
+            "mount",
+            "-o",
+            "vers=3,sec=sys",
+            (char *)nfsvolume,
+            (char *)mountpoint,
+            NULL
+        };
+
+        (void) ct_tmpl_clear(tmplfd);
+        (void) close(tmplfd);
+
+        makePath(NFS_MOUNT, cmd, sizeof (cmd));
+
+        execv(cmd, argv);
+        fatal(ERR_EXEC_FAILED, "execve(%s) failed: %s\n", cmd, strerror(errno));
+    }
+
+    /* parent */
+    (void) ct_tmpl_clear(tmplfd);
+    (void) close(tmplfd);
+
+    dlog("INFO started mount[%d]\n", (int)pid);
+
+    while (wait(&status) != pid) {
+        /* EMPTY */;
+    }
+
+    if (WIFEXITED(status)) {
+        dlog("INFO mount[%d] exited: %d\n", (int)pid, WEXITSTATUS(status));
+    } else if (WIFSIGNALED(status)) {
+        fatal(ERR_EXEC_FAILED, "mount[%d] died on signal: %d\n",
+            (int)pid, WTERMSIG(status));
+    } else {
+        fatal(ERR_EXEC_FAILED, "mount[%d] failed in unknown way\n",
+            (int)pid);
+    }
+
+    return;
+}
+
+void
+mountNfsVolume(nvlist_t *data)
+{
+    char *nfsvolume, *mountpoint;
+    int ret;
+
+    ret = nvlist_lookup_string(data, "nfsvolume", &nfsvolume);
+    if (ret == 0) {
+        ret = nvlist_lookup_string(data, "mountpoint", &mountpoint);
+        if (ret == 0) {
+            doNfsMount((const char *)nfsvolume, (const char *)mountpoint);
+            return;
+        }
+    }
+
+    fatal(ERR_INVALID_NFS_VOLUMES, "invalid nfsvolumes");
+}
+
+void
+mountNfsVolumes()
+{
+    char *json;
+    nvlist_t *data, *nvl;
+    nvpair_t *pair;
+
+    if ((json = mdataGet("docker:nfsvolumes")) == NULL) {
+        dlog("No docker:nfsvolumes, nothing to mount\n");
+        return;
+    }
+
+    sleep(20);
+
+    if (nvlist_parse_json(json, strlen(json), &nvl, NVJSON_FORCE_INTEGER,
+      NULL) != 0) {
+        fatal(ERR_PARSE_JSON, "failed to parse nvpair json"
+            " for docker:nfsvolumes: %s\n", strerror(errno));
+    }
+    free(json);
+
+    // HACKS to workaround bugs
+    workaround_OS_5221();
+
+    for (pair = nvlist_next_nvpair(nvl, NULL); pair != NULL;
+      pair = nvlist_next_nvpair(nvl, pair)) {
+        if (nvpair_type(pair) == DATA_TYPE_NVLIST) {
+            if (nvpair_value_nvlist(pair, &data) != 0) {
+                fatal(ERR_PARSE_JSON, "failed to parse nvpair json"
+                    " for NFS volume: %s\n", strerror(errno));
+            }
+            mountNfsVolume(data);
+        }
+    }
+
+    nvlist_free(nvl);
+}
+
 long long
 currentTimestamp()
 {
@@ -1372,6 +1537,9 @@ main(int __attribute__((unused)) argc, char __attribute__((unused)) *argv[])
     killIpmgmtd();
 
     dlog("INFO network setup complete\n");
+
+    /* EXPERIMENTAL */
+    mountNfsVolumes();
 
     /* NOTE: all of these will call fatal() if there's a problem */
     setupHostname();

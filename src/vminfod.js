72b28b4b7c772e6099c3a2bd5ccc9b94e25145d4
Dave Eddy <dave@daveeddy.com>
2016-05-26 23:31:21 -0400

    vminfod command

---
+++ src/vminfod.js
@@ -0,0 +1,86 @@
+#!/usr/node/bin/node
+
+var f = require('util').format;
+
+var vminfod = require('/usr/vm/node_modules/vminfod/client');
+var client = new vminfod.VminfodClient();
+
+function usage() {
+    var _args = Array.prototype.slice.call(arguments);
+    var msg = f.apply(null, _args);
+    console.log([
+        msg,
+        '',
+        'Usage: vminfod [command] [args]',
+        '',
+        'Commands',
+        '  data               get all vminfod data (GET /data)',
+        '  status             get vminfod status   (GET /status)',
+        '  vms                get all vms          (GET /vms)',
+        '  vm <uuid>          get vm info          (GET /vms/:uuid)',
+        '  events [-j]        trace vminfod events (GET /events)'
+    ].join('\n'));
+}
+
+var args = process.argv.slice(2);
+var cmd = args.shift();
+
+switch (cmd) {
+    case 'status':
+    case 'vms':
+    case 'data':
+        client[cmd](function (err, out) {
+            if (err)
+                throw err;
+            console.log(JSON.stringify(out, null, 2));
+        });
+        break;
+    case 'vm':
+        var vm = args[0];
+        if (!vm) {
+            console.error('uuid argument missing');
+            process.exit(1);
+        }
+        client.vm(vm, function (err, out) {
+            if (err)
+                throw err;
+            console.log(JSON.stringify(out, null, 2));
+        });
+        break;
+    case 'events':
+        var vs = client.createEventStream();
+        vs.on('readable', function () {
+            var ev;
+            while ((ev = vs.read()) !== null) {
+                delete ev.vm;
+
+                if (args[0] === '-j' || args[0] === '--json') {
+                    console.log(JSON.stringify(ev));
+                    return;
+                }
+
+                // format the output nicely
+                var base = f('[%s] %s %s',
+                    ev.ts.toISOString(), ev.zonename, ev.type);
+
+                if (ev.changes) {
+                    ev.changes.forEach(function (change) {
+                        var s = f('%s %s :: %j -> %j',
+                            change.path, change.action, change.from, change.to);
+                        console.log('%s: %s', base, s);
+                    });
+                } else {
+                    console.log(base);
+                }
+            }
+        });
+        break;
+    case undefined:
+        usage('Command be specified as the first argument');
+        process.exit(1);
+        break;
+    default:
+        usage('Unknown command: %s', cmd);
+        process.exit(1);
+        break;
+}

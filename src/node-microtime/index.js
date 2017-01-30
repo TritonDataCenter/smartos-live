73d8c13dc6b3d6923a4a1a6203517f6cb568ccb5
Cody Peter Mello <cody.mello@joyent.com>
2017-01-30 07:39:37 +0000

    OS-5657 Remove unused node bindings from platform Reviewed by: Dave Eddy <dave@daveeddy.com> Approved by: Joshua M. Clulow <jmc@joyent.com>

--- src/node-microtime/index.js
+++
@@ -1,5 +0,0 @@
-var binding = require('bindings')('microtime.node')
-
-exports.now = binding.now
-exports.nowDouble = binding.nowDouble
-exports.nowStruct = binding.nowStruct
--- src/node-zutil/lib/zutil.js
+++
@@ -1,3 +0,0 @@
-// Copyright 2011 Mark Cavage <mcavage@gmail.com> All rights reserved.
-
-module.exports = require('./zutil_bindings');

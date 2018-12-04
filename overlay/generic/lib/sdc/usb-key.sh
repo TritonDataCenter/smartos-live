27f20be46bb0683dd8e6ddcacef2d69f49d8ed1a
John Levon <john.levon@joyent.com>
2018-12-04 16:52:35 +0000

    Add routine for mounting the ESP; a few minor fixes

--- overlay/generic/lib/sdc/usb-key.sh
+++ overlay/generic/lib/sdc/usb-key.sh
@@ -55,6 +55,7 @@ function mount_usb_key()
 	fi
 
 	if [[ -f "$mnt/.joyliveusb" ]]; then
+		echo $mnt
 		return 0
 	fi
 
@@ -74,7 +75,7 @@ function mount_usb_key()
 		*) continue ;;
 		esac
 
-		if [[ "$(/usr/sbin/fstyp $devpath)" != "pcfs" ]]; then
+		if [[ "$(/usr/sbin/fstyp $devpath 2>/dev/null)" != "pcfs" ]]; then
 			continue
 		fi
 
@@ -119,4 +120,70 @@ function unmount_usb_key()
 	fi
 
 	umount "$mnt"
+}
+
+function unmount_usb_key()
+{
+	local readonly mnt=$1
+
+	if [[ -z "$mnt" ]]; then
+		mnt=/mnt/$(svcprop -p "joyentfs/usb_mountpoint" \
+		    "svc:/system/filesystem/smartdc:default")
+	fi
+
+	typ=$(awk -v "mnt=$mnt" '$2 == mnt { print $3 }' /etc/mnttab)
+
+	[[ -z $typ ]] && return 0
+
+	if [[ ! -f "$mnt/.joyliveusb" ]]; then
+		echo "$mnt does not contain a USB key" >&2
+		return 1
+	fi
+
+	umount "$mnt"
+}
+
+#
+# Mount the EFI system partition, if there is one.  Note that since we need to
+# peek at .joyliveusb to be sure, the only way to find a USB key is to mount its
+# root first...
+#
+function mount_usb_key_esp()
+{
+	rootmnt=$(mount_usb_key)
+
+	if [[ $? -ne 0 ]]; then
+		return 1
+	fi
+
+	dev=$(mount | nawk "\$0~\"^$rootmnt\" { print \$3 ; }")
+	dsk=${dev%[ps]?}
+
+	mnt=/tmp/mnt.$$
+
+	if ! mkdir -p $mnt; then
+		echo "failed to mkdir $mnt" 2>&1
+		return 1
+	fi
+
+	version=$(usb_key_version ${dsk}p0)
+
+	#
+	# If this key is still grub, then we don't have an ESP, but we shouldn't
+	# report an error.
+	#
+	if [[ "$version" = "1" ]]; then
+		rmdir $mnt
+		return 0
+	fi
+
+	/usr/sbin/mount -F pcfs -o foldcase,noatime ${dsk}s0 $mnt
+
+	if [[ $? -ne 0 ]]; then
+		rmdir $mnt
+		return 1
+	fi
+
+	echo $mnt
+	return 0
 }

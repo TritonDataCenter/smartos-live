8c2a9db7f7c07503a1f9de0f0159fd3df9d0abdc
Dan McDonald <danmcd@joyent.com>
2021-01-14 23:54:25 -0500

    OS-8261 piadm(1M) shouldn't case-correct already-correct os/ entries

--- src/piadm.sh
+++ src/piadm.sh
@@ -1015,7 +1015,11 @@ bringup_HN() {
 	vecho "Case-correcting os/ entries."
 	cd ./os
 	for a in *; do
-		mv "$a" "${a^^}"
+		# If our "usb key" is already a ZFS bootfs, don't mv
+		# identically-named things.
+		if [[ "$a" != "${a^^}" ]]; then
+			mv "$a" "${a^^}"
+		fi
 	done
 
 	if [[ "$stick_premounted" != "yes" ]]; then

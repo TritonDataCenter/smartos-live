b8f78c273313d8b2d9d881fac3e779289ba350bf
Brian Bennett <brian.bennett@mnx.io>
2023-09-01 09:17:06 -0700

    Address feedbck.

--- src/smartdc/lib/ntp_config.js
+++ src/smartdc/lib/ntp_config.js
@@ -2,6 +2,27 @@
 /* vim: syn=javascript ts=8 sts=8 sw=8 noet: */
 
 /*
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License, Version 1.0 only
+ * (the "License").  You may not use this file except in compliance
+ * with the License.
+ *
+ * You can obtain a copy of the license at http://smartos.org/CDDL
+ *
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file.
+ *
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ *
  * Copyright 2019 Joyent, Inc.
  * Copyright 2023 MNX Cloud, Inc.
  */
@@ -206,7 +227,7 @@ generate_file(servers, allowed_subnets, trusted, orphan)
 			}
 
 			/*
-			 * We should deduplicate these networks since the -a
+			 * Now deduplicate these networks since the -a
 			 * flag can be passed multiple times.
 			 */
 			if (seen.indexOf(as[0] + '/' + mask) === -1) {

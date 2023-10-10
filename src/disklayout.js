5ccd0104c22267e7a7272dfa560a06850ef10ef3
Travis Paul <travis.paul@mnx.io>
2023-10-10 16:33:06 -0500

    Filter out zvol devices before comuting ideal disklayout
    This is only useful after a CN has already been setup but better
    matches the SmartOS behavior.

--- src/disklayout.js
+++ src/disklayout.js
@@ -47,11 +47,21 @@ dolayout(disks, layout, nspares, excluded, enable_cache, width)
 			return (true);
 		});
 	} else if (os.type() === 'Linux') {
+		var zvolExp = /^zd[0-9a-z]*$/;
 		disks = disks.filter(function (disk) {
 			if (excluded.indexOf(disk.name.toLowerCase()) !== -1) {
 				return (false);
 			}
 
+			// Filter out any zvol devices
+			// Alternatively we could pass -e MAJOR_NUM to lsblk but would need
+			// to parse /proc/devices to get the major device number for zvols
+			if (zvolExp.test(disk.name)) {
+				return (false);
+			}
+
+			// Filter out any null values from the mountpoints array
+			// If a disk has no mountpoints the array has a single null item
 			var mountpoints = disk.mountpoints.filter(function (mountpoint) {
 				return (mountpoint !== null);
 			});

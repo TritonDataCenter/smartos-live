16fa7fc664b44becddf7b53ba6f30982db5a9c60
Brian Bennett <brian.bennett@mnx.io>
2024-01-09 12:18:57 -0800

    Update this other comment too

--- src/lib/sdc/usb-key.sh
+++ src/lib/sdc/usb-key.sh
@@ -228,8 +228,9 @@ function mount_usb_key()
 	if /bin/bootparams | grep -q "^triton_installer=" ; then
 		# Technically we shouldn't ever see "skip" here
 		# because the only caller of mount_usb_key() with skip
-		# is piadm(8)'s `install`, which can't be invoked on
-		# a Triton Head Node.  Checking to be safe.
+		# is piadm(8)'s `install`, or smartos_prompt_config.sh, neither
+		# of which can be invoked on a Triton Head Node.  But checking
+		# anyway, just to be safe.
 		if [[ "$2" == "skip" ]]; then
 			echo "Somehow a piadm(8) install on a Head Node is" \
 				"happening. This is disallowed." >&2
